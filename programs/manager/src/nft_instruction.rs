15ccb19b7e07c23f0093582545bd836cc122de2b
Egor Parfenenko <egorparfenenko.work@gmail.com>
2025-10-14 16:19:47 +0200

    feat(manager): implement program

---
+++ programs/manager/src/nft_instruction.rs
@@ -0,0 +1,42 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub enum NftMinterInstruction {
+    InitializeConfig,
+    MintContributorBadge,
+}
+
+pub fn mint_badge_ix(
+    program_id: Pubkey,
+    admin: Pubkey,
+    config_pda: Pubkey,
+    mint_account: Pubkey,
+    mint_authority_pda: Pubkey,
+    recipient: Pubkey,
+    token_account: Pubkey,
+    token_program: Pubkey,
+    system_program: Pubkey,
+    rent_sysvar: Pubkey,
+) -> Instruction {
+    Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new_readonly(admin, true),
+            AccountMeta::new_readonly(config_pda, false),
+            AccountMeta::new(mint_account, false),
+            AccountMeta::new(mint_authority_pda, false),
+            AccountMeta::new(recipient, false),
+            AccountMeta::new(token_account, false),
+            AccountMeta::new_readonly(token_program, false),
+            AccountMeta::new_readonly(system_program, false),
+            AccountMeta::new_readonly(rent_sysvar, false),
+        ],
+        data: NftMinterInstruction::MintContributorBadge
+            .try_to_vec()
+            .unwrap(),
+    }
+}
---
+++ programs/manager/src/treasury_instruction.rs
@@ -0,0 +1,50 @@
+use borsh::BorshSerialize;
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize)]
+pub enum TreasuryInstruction {
+    CreateVault,
+    DepositFunds { amount: u64 },
+}
+
+pub fn create_vault_ix(
+    program_id: Pubkey,
+    payer: Pubkey,
+    campaign: Pubkey,
+    vault: Pubkey,
+) -> Instruction {
+    Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(campaign, false),
+            AccountMeta::new(vault, false),
+            AccountMeta::new_readonly(solana_program::system_program::ID, false),
+        ],
+        data: TreasuryInstruction::CreateVault.try_to_vec().unwrap(),
+    }
+}
+
+pub fn deposit_funds_ix(
+    program_id: Pubkey,
+    contributor: Pubkey,
+    vault: Pubkey,
+    campaign: Pubkey,
+    amount: u64,
+) -> Instruction {
+    Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(contributor, true),
+            AccountMeta::new(vault, false),
+            AccountMeta::new_readonly(campaign, false),
+            AccountMeta::new_readonly(solana_program::system_program::ID, false),
+        ],
+        data: TreasuryInstruction::DepositFunds { amount }
+            .try_to_vec()
+            .unwrap(),
+    }
+}
