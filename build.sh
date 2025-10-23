99972cb75052c802d6458d1769ff70386704017c
Yegor Parfenenka <egor.parf@coinchange.io>
2025-10-23 12:23:24 +0200

    fix(build.sh): use seer-agave for program builds

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -e
 
 # solana programs
 AGAVE=../agave
-cargo build-sbf --sbf-sdk $AGAVE/platform-tools-sdk/sbf --debug --manifest-path programs/manager/Cargo.toml
-cargo build-sbf --sbf-sdk $AGAVE/platform-tools-sdk/sbf --debug --manifest-path programs/nftminter/Cargo.toml
-cargo build-sbf --sbf-sdk $AGAVE/platform-tools-sdk/sbf --debug --manifest-path programs/treasury/Cargo.toml
+$AGAVE/target/debug/cargo-build-sbf --sbf-sdk $AGAVE/platform-tools-sdk/sbf --debug --manifest-path programs/manager/Cargo.toml
+$AGAVE/target/debug/cargo-build-sbf --sbf-sdk $AGAVE/platform-tools-sdk/sbf --debug --manifest-path programs/nftminter/Cargo.toml
+$AGAVE/target/debug/cargo-build-sbf --sbf-sdk $AGAVE/platform-tools-sdk/sbf --debug --manifest-path programs/treasury/Cargo.toml
 
 # client
 RUSTFLAGS="-A warnings" cargo build --package client
